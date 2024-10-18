03557c02c12f17549a7685ae997de3703fb9835f
suradede34 <suradedeyy@gmail.com>
2024-10-18 17:27:15 +0300

    Add files via upload

---
+++ assets/js/10soru.js
@@ -0,0 +1,74 @@
+
+//10 SORU SORMA
+
+let soru1 = prompt("Dunyanin en yuksek dagı Everest midir? ");
+let soru2 = prompt("En buyuk okyanus Pasifik midir? ");
+let soru3 = prompt("Sahara Colu Afrika'da midir? ");
+let soru4 = prompt("Dunyanin en buyuk golu Kaspiyen Golu mudur? ");
+let soru5 = prompt("Amazon Nehri dünyanin en uzun nehri midir? ");
+let soru6 = prompt("Hindistan Asya'da midir? ");
+let soru7 = prompt("Cin en kalabalik ulke midir? ");
+let soru8 = prompt("Gronland dunyanin en buyuk adasi midir? ");
+let soru9 = prompt("Misir'in baskenti Kahire midir? ");
+let soru10 = prompt("Mariana cukuru dunyanin en derin noktasi midir? ");
+
+let dogruSayisi = 0; 
+let yanlisSayisi = 0;
+
+if(soru1 === "evet") {
+    console.log(true)
+} else{
+    console.log(false)
+}
+if(soru2 === "evet") {
+    console.log(true)
+} else{
+    console.log(false)
+}
+if(soru3 === "evet") {
+    console.log(true)
+} else{
+    console.log(false)
+}
+if(soru4 === "evet") {
+    console.log(true)
+} else{
+    console.log(false)
+}
+if(soru5 === "hayir") {
+    console.log(true)
+} else{
+    console.log(false)
+}
+if(soru6 === "evet") {
+    console.log(true)
+} else{
+    console.log(false)
+}
+if(soru7 === "evet") {
+    console.log(true)
+} else{
+    console.log(false)
+}
+if(soru8 === "evet") {
+    console.log(true)
+} else{
+    console.log(false)
+}
+if(soru9 === "evet") {
+    console.log(true)
+} else{
+    console.log(false)
+}
+if(soru10 === "evet") {
+    console.log(true)
+} else{
+    console.log(false)
+}
+
+console.log("Doğru cevap sayısı: " + dogruSayisi);
+console.log("Yanlış cevap sayısı: " + yanlisSayisi);
+
+  
+  
+                                 
